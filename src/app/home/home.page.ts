b4cd22d8e69dad28bda93250717757e47ab3460e
NostosP <paruta.info@gmail.com>
2019-02-04 14:21:16 +0100

    Add order by functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { Contact } from './../models/contact';
 import { ContactService } from './../services/contact.service';
 import { Component, OnInit } from '@angular/core';
 import { AlertController, NavController } from '@ionic/angular';
@@ -12,6 +13,7 @@ export class HomePage implements OnInit {
 
   searchTerm = '';
   contacts: any = [];
+  orderValue = '';
 
   constructor(public alertController: AlertController,
               public contactService: ContactService,
@@ -28,7 +30,61 @@ export class HomePage implements OnInit {
     this.contactService.filterContacts(this.searchTerm)
       .then((filteredContacts) => {
         this.contacts = filteredContacts;
+        this.orderContacts();
       });
+  }
+
+  /**
+   * Order contacts depending on the selected order option,
+   * default is by first name
+   */
+  orderContacts() {
+    switch (this.orderValue) {
+      case 'f': {
+        this.contacts.sort(this.compareByFirstName);
+        break;
+      }
+      case 'l': {
+        this.contacts.sort(this.compareByLastName);
+        break;
+      }
+      case 'e': {
+        this.contacts.sort(this.compareByEmail);
+        break;
+      }
+      case 'p': {
+        this.contacts.sort(this.compareByPhone);
+        break;
+      }
+      default: {
+        this.contacts.sort(this.compareByFirstName);
+        break;
+      }
+    }
+  }
+
+  compareByFirstName(a: Contact, b: Contact) {
+    if (a.firstName < b.firstName) {return -1; }
+    if (a.firstName > b.firstName) {return 1; }
+    return 0;
+  }
+
+  compareByLastName(a: Contact, b: Contact) {
+    if (a.lastName < b.lastName) {return -1; }
+    if (a.lastName > b.lastName) {return 1; }
+    return 0;
+  }
+
+  compareByEmail(a: Contact, b: Contact) {
+    if (a.email < b.email) {return -1; }
+    if (a.email > b.email) {return 1; }
+    return 0;
+  }
+
+  compareByPhone(a: Contact, b: Contact) {
+    if (a.phone < b.phone) {return -1; }
+    if (a.phone > b.phone) {return 1; }
+    return 0;
   }
 
   /**
